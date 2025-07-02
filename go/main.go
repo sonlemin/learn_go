f7ea142b1c66e42d993b289588ad5c4d4fcc9029
sonlemin <sonlm2005@icloud.com>
2025-07-02 18:44:23 +0700

    3rd

--- go/main.go
+++ go/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
-
-func main() {
-	if length := 10; length < 100 {
-		fmt.Println("Email invalid")
+func getMonthlyPrice(tier string) int {
+	switch tier {
+	case "basic":
+		return 100 * 100
+	case "premium":
+		return 150 * 100
+	case "enterprise":
+		return 500 * 100
+	default:
+		return 0
 	}
 }
--- go/test.go
+++ go/test.go
@@ -5,26 +5,21 @@ import (
 	"testing"
 )
 
-func getMessageText(m messageToSend) string {
-	return fmt.Sprintf("Sending message: '%s' to: %v", m.message, m.phoneNumber)
-}
-
 func Test(t *testing.T) {
 	type testCase struct {
-		phoneNumber int
-		message     string
-		expected    string
+		tier     string
+		expected int
 	}
 
 	runCases := []testCase{
-		{148255510981, "Thanks for signing up", "Sending message: 'Thanks for signing up' to: 148255510981"},
-		{148255510982, "Love to have you aboard!", "Sending message: 'Love to have you aboard!' to: 148255510982"},
+		{"basic", 10000},
+		{"premium", 15000},
+		{"enterprise", 50000},
 	}
 
 	submitCases := append(runCases, []testCase{
-		{148255510983, "We're so excited to have you", "Sending message: 'We're so excited to have you' to: 148255510983"},
-		{148255510984, "", "Sending message: '' to: 148255510984"},
-		{148255510985, "Hello, World!", "Sending message: 'Hello, World!' to: 148255510985"},
+		{"invalid", 0},
+		{"", 0},
 	}...)
 
 	testCases := runCases
@@ -38,26 +33,23 @@ func Test(t *testing.T) {
 	failCount := 0
 
 	for _, test := range testCases {
-		output := getMessageText(messageToSend{
-			phoneNumber: test.phoneNumber,
-			message:     test.message,
-		})
+		output := getMonthlyPrice(test.tier)
 		if output != test.expected {
 			failCount++
 			t.Errorf(`---------------------------------
-Inputs:     (%v, %v)
+Inputs:     (%v)
 Expecting:  %v
 Actual:     %v
 Fail
-`, test.phoneNumber, test.message, test.expected, output)
+`, test.tier, test.expected, output)
 		} else {
 			passCount++
 			fmt.Printf(`---------------------------------
-Inputs:     (%v, %v)
+Inputs:     (%v)
 Expecting:  %v
 Actual:     %v
 Pass
-`, test.phoneNumber, test.message, test.expected, output)
+`, test.tier, test.expected, output)
 		}
 	}
 
